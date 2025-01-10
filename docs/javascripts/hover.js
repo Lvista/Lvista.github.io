0c732768ed3608ca255afcf1985739c05607d8f8
Lvista <532177525@qq.com>
2025-01-10 16:07:46 +0900

    delete all redundant file and setting

--- docs/javascripts/hover.js
+++
@@ -1,9 +0,0 @@
-const container = document.querySelector('.flip-container');
-
-container.addEventListener('mouseover', () => {
-    container.classList.add('hover');
-});
-
-container.addEventListener('mouseout', () => {
-    container.classList.remove('hover');
-});
